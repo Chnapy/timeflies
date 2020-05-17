a369bde4e38502dd717b06533fb1c79badc471cb
Chnapy <richardhaddad@hotmail.fr>
2020-05-17 20:20:53 +0200

    fix body scrollbar

--- packages/front/src/ui/ui-provider.tsx
+++ packages/front/src/ui/ui-provider.tsx
@@ -7,6 +7,9 @@ const useStyles = makeStyles(() => ({
     '@global': {
         html: {
             fontSize: '62.5%'
+        },
+        body: {
+            overflow: 'hidden'
         }
     }
 }));
