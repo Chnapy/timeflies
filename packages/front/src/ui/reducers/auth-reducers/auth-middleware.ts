5d9701201b7f909332869c0e06d8792d0b72e993
Chnapy <richardhaddad@hotmail.fr>
2020-08-01 22:12:51 +0200

    improve auth management with error handling and disconnects handling

--- packages/front/src/ui/reducers/auth-reducers/auth-middleware.ts
+++ packages/front/src/ui/reducers/auth-reducers/auth-middleware.ts
@@ -1,48 +1,51 @@
 import { Middleware, MiddlewareAPI } from "@reduxjs/toolkit";
-import { AuthResponseBody, getEndpoint } from "@timeflies/shared";
+import { authEndpoint, AuthRequestBody, AuthResponseBody, getEndpoint } from "@timeflies/shared";
 import { envManager } from "../../../envManager";
 import { AuthFormSubmit, AuthHttpSuccess } from "./auth-actions";
 
-const endpoint = getEndpoint('http', envManager.REACT_APP_SERVER_URL);
+const authHref = new URL(
+    authEndpoint,
+    getEndpoint('http', envManager.REACT_APP_SERVER_URL)
+).href;
 
 export const authMiddleware: Middleware = (api: MiddlewareAPI) => (next) => {
 
     const submitForm = async ({ payload }: AuthFormSubmit) => {
         const { playerName } = payload;
 
-        // TODO use url-join or others
-        const res = await fetch(endpoint + '/auth', {
+        const bodyRaw: AuthRequestBody = {
+            playerName
+        };
+
+        const res = await fetch(authHref, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                playerName
-            }),
+            body: JSON.stringify(bodyRaw),
         });
 
         if (res.ok) {
 
             const resBody: AuthResponseBody = await res.json();
 
-            if (resBody.success) {
+            const { token } = resBody;
 
-                const { token } = resBody;
-
-                await api.dispatch(AuthHttpSuccess({
-                    playerName,
-                    token
-                }));
-            }
+            await api.dispatch(AuthHttpSuccess({
+                playerName,
+                token
+            }));
 
         } else {
 
             console.error('auth failed');
             try {
-                console.error(await res.json());
+                const resBody: AuthResponseBody = await res.json();
 
-                // TODO handle errors
-            } catch (e) { }
+                return Promise.reject(resBody.error);
+            } catch (e) {
+                return Promise.reject();
+            }
         }
     };
 
