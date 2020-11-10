a5c5caf01acf44c78dda7c7f29822f92d54bdb05
Chnapy <richardhaddad@hotmail.fr>
2020-11-10 20:08:08 +0100

    improve react props

--- packages/animated-complex-sprite/src/react-props.ts
+++ packages/animated-complex-sprite/src/react-props.ts
@@ -7,51 +7,48 @@ type AnimatedComplexSpritePrimaryProps<S> = {
     getFramesInfos: FramesInfosGetter<S>;
     state: S;
     ticker?: Ticker;
-    cache: Cache<S, TexturesInfos>;
+    cache?: Cache<S, TexturesInfos>;
 };
 
-type AnimatedComplexSpriteSecondaryProps = {
+type AnimatedComplexSpriteSecondaryProps<S> = Pick<AnimatedComplexSprite<S>, 'onLoop' | 'onFrameChange'> & {
     run?: boolean;
+    keepTimeState?: boolean;
 };
 
 export type AnimatedComplexSpriteProps<S = any> = AnimatedComplexSpritePrimaryProps<S>
-    & AnimatedComplexSpriteSecondaryProps;
+    & AnimatedComplexSpriteSecondaryProps<S>;
 
-const denyUpdateProps = <P, K extends keyof P>(oldProps: P, newProps: P, ...keys: K[]): Omit<P, K> => {
+const denyUpdateProps = <P, K extends keyof P>(oldProps: P, newProps: P, ...keys: K[]) => {
     const unexpectedPropsChanges = keys.filter(key => oldProps[ key ] !== newProps[ key ]);
     if (unexpectedPropsChanges.length) {
         throw new Error(`Unexpected props change. These props should NOT change: ${unexpectedPropsChanges.join(', ')}`);
     }
-    return newProps;
 };
 
 export const getAnimatedComplexSpriteFromProps = ({
-    spritesheet, getFramesInfos, state, ticker, cache, run
-}: AnimatedComplexSpriteProps) => {
-
-    const sprite = new AnimatedComplexSprite(spritesheet, getFramesInfos, state, ticker, cache);
-
-    if (run) {
-        sprite.play();
-    } else if (run === false) {
-        sprite.stop();
-    }
-
-    return sprite;
-};
+    spritesheet, getFramesInfos, state, ticker, cache
+}: AnimatedComplexSpriteProps) => new AnimatedComplexSprite(spritesheet, getFramesInfos, state, ticker, cache);
 
 export const applyAnimayedComplexSpriteProps = (
     sprite: AnimatedComplexSprite,
-    oldProps: AnimatedComplexSpriteProps,
+    oldProps: AnimatedComplexSpriteProps | undefined,
     newProps: AnimatedComplexSpriteProps
 ) => {
-    const { state, run } = denyUpdateProps(oldProps, newProps, 'spritesheet', 'getFramesInfos', 'ticker', 'cache');
-
-    sprite.setState(state);
-
-    if(run) {
-        sprite.play();
-    } else if(run === false) {
-        sprite.stop();
+    if (oldProps) {
+        denyUpdateProps(oldProps, newProps, 'spritesheet', 'getFramesInfos', 'ticker', 'cache');
     }
+    const { state, onLoop, onFrameChange, run, keepTimeState } = newProps;
+
+    sprite.setState(state, {
+        keepTimeState
+    });
+
+    if (run === false) {
+        sprite.stop();
+    } else {
+        sprite.play();
+    }
+
+    sprite.onLoop = onLoop;
+    sprite.onFrameChange = onFrameChange;
 };
