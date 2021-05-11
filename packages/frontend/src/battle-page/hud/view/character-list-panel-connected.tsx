500a621c08bb117a7bc4578164595f82b2c64bb5
Richard Haddad <richardhaddad@hotmail.fr>
2021-05-11 23:46:38 +0200

    upgrade deps minor versions

--- packages/frontend/src/battle-page/hud/view/character-list-panel-connected.tsx
+++ packages/frontend/src/battle-page/hud/view/character-list-panel-connected.tsx
@@ -1,7 +1,6 @@
 import { Grid } from '@material-ui/core';
 import { CharacterListPanel } from '@timeflies/character-list-panel';
 import React from 'react';
-import { useBattleSelector } from '../../store/hooks/use-battle-selector';
 import { CharacterDetailsPanel } from '../details-panel/character-details-panel';
 import { DetailsContextProvider } from '../details-panel/details-context';
 import { SpellDetailsPanel } from '../details-panel/spell-details-panel';
@@ -16,26 +15,20 @@ export const InnerCharacterListPanel: React.FC = () => {
     );
 };
 
-export const CharacterListPanelConnected: React.FC = () => {
-
-    const selectedCharacterId = useBattleSelector(state => state.characterList[ 0 ]);
-    const selectedSpellId = useBattleSelector(state => state.spellLists[ selectedCharacterId ][ 0 ]);
-
-    return (
-        <DetailsContextProvider>
-            <Grid container alignItems='flex-end' spacing={1}>
-                <Grid item>
-                    <InnerCharacterListPanel />
-                </Grid>
-
-                <Grid item>
-                    <CharacterDetailsPanel />
-                </Grid>
-
-                <Grid item>
-                    <SpellDetailsPanel />
-                </Grid>
+export const CharacterListPanelConnected: React.FC = () => (
+    <DetailsContextProvider>
+        <Grid container alignItems='flex-end' spacing={1}>
+            <Grid item>
+                <InnerCharacterListPanel />
             </Grid>
-        </DetailsContextProvider>
-    );
-};
+
+            <Grid item>
+                <CharacterDetailsPanel />
+            </Grid>
+
+            <Grid item>
+                <SpellDetailsPanel />
+            </Grid>
+        </Grid>
+    </DetailsContextProvider>
+);
