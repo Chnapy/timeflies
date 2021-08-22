a2f3186d2e63ec21acc4fa1cd956918efcdbdd88
Richard Haddad <richardhaddad@hotmail.fr>
2021-08-22 02:26:09 +0200

    add PWA and theme color

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,3 +15,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
