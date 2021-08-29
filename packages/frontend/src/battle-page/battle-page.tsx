535c251ae771e716fd68a4e96850d6cbf5fd1021
Richard Haddad <richardhaddad@hotmail.fr>
2021-08-29 19:03:25 +0200

    change mobile orientation depending on page (#164)

--- packages/frontend/src/battle-page/battle-page.tsx
+++ packages/frontend/src/battle-page/battle-page.tsx
@@ -1,13 +1,14 @@
 import { Container, makeStyles } from '@material-ui/core';
+import { usePlayMusic } from '@timeflies/app-ui';
 import { useSocketSend } from '@timeflies/socket-client';
 import { BattleLeaveMessage, BattleLoadMessage } from '@timeflies/socket-messages';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import useAsyncEffect from 'use-async-effect';
-import { usePlayMusic } from '@timeflies/app-ui';
 import { LoadingBackdrop } from '../components/loading-backdrop';
 import { NotFoundPanel } from '../components/not-found-panel';
+import { useOrientationLockSetter } from '../components/orientation-lock/orientation-lock-context';
 import { useSocketSendWithResponseError } from '../connected-socket/hooks/use-socket-send-with-response-error';
 import { useGameSelector } from '../store/hooks/use-game-selector';
 import { BattleView } from './battle-view';
@@ -27,6 +28,8 @@ const useStyles = makeStyles(() => ({
 export const BattlePage: React.FC = () => {
     const classes = useStyles();
     usePlayMusic('battle');
+    useOrientationLockSetter(true);
+
     const { battleId } = useParams<BattlePageParams>();
     const hasBattleData = useGameSelector(state => state.battle !== null);
     const send = useSocketSend();
